0c8a301df1b4bd6a798abb6e66ddf85c915bf1d4
AbdullahKus <62968246+abdks@users.noreply.github.com>
2023-11-25 00:05:55 +0300

    25 kasim

--- MyAcademyCarBook.PresentationLayer/Controllers/CarController.cs
+++ MyAcademyCarBook.PresentationLayer/Controllers/CarController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyAcademyCarBook.BusinessLayer.Abstract;
-
+using X.PagedList;
 namespace MyAcademyCarBook.PresentationLayer.Controllers
 {
     public class CarController : Controller
@@ -24,13 +25,18 @@ namespace MyAcademyCarBook.PresentationLayer.Controllers
             var values = _carService.TGetAllCarsWithBrands();
             return View(values);
         }
-        public IActionResult CarList() 
+        public IActionResult CarList(int page = 1) 
         {
 
             ViewBag.title1 = "Araba Listesi";
             ViewBag.title2 = "Sizin İçin Araç Listemiz";
-            var values = _carService.TGetAllCarsWithBrands();
+            //var values = _carService.TGetAllCarsWithBrands();
+            //return View(values).ToPagedList(a,3);
+            var result = _carService.TGetAllCarsWithBrands();
+
+            var values = result.ToPagedList(page, 3);
             return View(values);
+
         }
         public IActionResult CarDetail(int id)
         {
