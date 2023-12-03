3c0b54e0319d590d1e592439daaa26abe8a10051
AbdullahKus <62968246+abdks@users.noreply.github.com>
2023-12-03 19:03:40 +0300

    3aralik

---
+++ MyAcademyCarBook.PresentationLayer/Controllers/DashboardController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAcademyCarBook.DataAccessLayer.Abstract;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System.Collections.Generic;
+
+
+
+namespace MyAcademyCarBook.PresentationLayer.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ICarDal _carDal;
+        private readonly ICarCategoryDal _categoryDal;
+        private readonly IBrandDal _brandDal;
+        private readonly IContactDal _contactDal;
+        public DashboardController(ICarDal carDal, ICarCategoryDal categoryDal, IBrandDal brandDal, IContactDal contactDal)
+        {
+            _carDal = carDal;
+            _categoryDal = categoryDal;
+            _brandDal = brandDal;
+            _contactDal = contactDal;
+        }
+
+        public class DashboardViewModel
+        {
+            public List<Car> Cars { get; set; }
+            public List<Contact> Contacts { get; set; }
+        }
+
+        public IActionResult Index()
+        {
+
+
+
+       
+        // View'e araç sayısını gönder
+        int carCount = _carDal.GetAll().Count;
+            int categoryCount = _categoryDal.GetAll().Count();
+            int brandCount = _brandDal.GetAll().Count();
+            int contactCount = _contactDal.GetAll().Count();
+
+            // View'e araç sayısını gönder
+            ViewBag.CarCount = carCount;
+            ViewBag.CategoryCount = categoryCount;
+            ViewBag.BrandCount = brandCount;
+            ViewBag.ContactCount = contactCount;
+
+            //Verileri Çek
+            var cars = _carDal.GetAll();
+            var contacts = _contactDal.GetAll();
+            var viewModel = new DashboardViewModel
+            {
+                Cars = cars,
+                Contacts = contacts
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
